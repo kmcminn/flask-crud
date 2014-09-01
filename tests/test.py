6183150658a860d0a77c342a960bfb11b082f5b1
kmcminn <tenyou@gmail.com>
2014-09-01 01:26:44 -0700

    simply tests for this app

--- tests/test.py
+++ tests/test.py
@@ -9,25 +9,24 @@ from flaskapi import app
 
 class BasicTestCase(unittest.TestCase):
 
-    def test_publish_invalid_json(self):
+    def test_publish_get_form(self):
         """Test status code 405 from improper JSON on post to raw"""
         tester = app.test_client(self)
-        response = tester.post('/api/publish', data="This isn't a json... it's a string!")
-        self.assertEqual(response.status_code, 405)
+        response = tester.get('/api/publish')
+        self.assertEqual(response.status_code, 200)
 
     def test_search_monitor_record_exists(self):
         """Test monitor object is present"""
         tester = app.test_client(self)
         response = tester.get('/monitor', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, "Hello World!")
+        self.assertEqual(response.data, "OK")
 
     def test_get_single_record_html(self):
         """Test monitor object is present"""
         tester = app.test_client(self)
-        response = tester.get('/api/get/1', content_type='html/text')
+        response = tester.get('/api/view/1', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, "Hello World!")
 
 if __name__ == '__main__':
     unittest.main()
