309560cff687a5f51144e061061a05a29f65d44b
kmcminn <tenyou@gmail.com>
2014-09-01 14:40:52 -0700

    reuse error.code instead

--- flaskapi/controllers/errors.py
+++ flaskapi/controllers/errors.py
@@ -1,10 +1,9 @@
-from flaskapi.util import *
 from flaskapi import app
 from flask import jsonify, request
 
 
 @app.errorhandler(404)
 def not_found(error=None):
-    response = jsonify({'status': 404, 'message': 'not found' + request.url})
-    response.status_code = 404
+    response = jsonify({'status': error.code, 'message': 'not found: ' + request.url})
+    response.status_code = error.code
     return response
