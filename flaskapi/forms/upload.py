d8e555cf60a9e097b3c7ca6102821f72085ff39a
kmcminn <tenyou@gmail.com>
2014-09-01 14:39:03 -0700

    form class into its own location

--- flaskapi/forms/upload.py
+++ flaskapi/forms/upload.py
@@ -1,14 +1,10 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
-    SubmitField
-from wtforms.validators import Required, Length, Email, Regexp
+from wtforms import StringField, SubmitField
+from wtforms.validators import Length
 from flask_wtf.file import FileField
-from wtforms import ValidationError
-from flask.ext.pagedown.fields import PageDownField
-from ..models import Role, User
 
-class EditProfileForm(Form):
-    name = StringField('filename', validators=[Length(0, 64)])
-    description = StringField('Location', validators=[Length(0, 64)])
-    n_file = FileField('Your File:')
+
+class FileForm(Form):
+    file = FileField('Your File:  ')
+    description = StringField('Description: ', validators=[Length(0, 30)])
     submit = SubmitField('Submit')
